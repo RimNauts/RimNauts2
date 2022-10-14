2234154f28093f42b49f10db16cdf21445fb3c39
Sindre Eiklid <sindre0830@gmail.com>
2022-10-14 18:01:53 +0200

    Fix NEOs not hiding properly

--- Source/RimNauts2/RimNauts2/World/WorldRendererUtility_HiddenBehindTerrainNow.cs
+++ Source/RimNauts2/RimNauts2/World/WorldRendererUtility_HiddenBehindTerrainNow.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using HarmonyLib;
 using RimWorld.Planet;
-using RimWorld;
 using Verse;
 using UnityEngine;
 
 namespace RimNauts2 {
     [HarmonyPatch(typeof(WorldRendererUtility), "HiddenBehindTerrainNow")]
     internal static class WorldRendererUtility_HiddenBehindTerrainNow {
-
         internal static void Postfix(Vector3 pos, ref bool __result) {
-            Vector3 normalized = pos.normalized;
             Vector3 currentlyLookingAtPointOnSphere = Find.WorldCameraDriver.CurrentlyLookingAtPointOnSphere;
-            float mag = pos.magnitude;
+            float distance = Vector3.Distance(pos, currentlyLookingAtPointOnSphere);
+            // ignore icons on surface
+            if (distance < 110) return;
+            // hide icons when zooming in
+            if ((int) Find.WorldCameraDriver.CurrentZoom != (int) WorldCameraZoomRange.VeryFar) {
+                __result = true;
+                return;
+            }
             float altper = Find.WorldCameraDriver.AltitudePercent;
+            float degree = 165 + (15 * altper);
             float minAlt = 125;
             float maxAlt = 1100;
             float alt = altper * (maxAlt - minAlt) + maxAlt;
-            __result = Vector3.Angle(normalized, currentlyLookingAtPointOnSphere) > (Math.Acos(115 / alt) + Math.Acos(115 / mag)) * (180 / 3.14);
-            if (mag < 115) __result = Vector3.Angle(normalized, currentlyLookingAtPointOnSphere) > 73f;
+            Vector3 normalized = pos.normalized;
+            __result = Vector3.Angle(normalized, currentlyLookingAtPointOnSphere) > (Math.Acos(115 / alt) + Math.Acos(115 / pos.magnitude)) * (degree / 3.14);
+            if (pos.magnitude < 115) __result = Vector3.Angle(normalized, currentlyLookingAtPointOnSphere) > 73f;
         }
     }
 }
