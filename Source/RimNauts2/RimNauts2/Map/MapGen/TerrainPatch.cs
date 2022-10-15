7c3bf6149f5eff3ab54c9d3b0169dce5428e00f8
Sindre Eiklid <sindre0830@gmail.com>
2022-10-15 07:00:56 +0200

    Remove unused files

--- Source/RimNauts2/RimNauts2/Map/MapGen/TerrainPatch.cs
+++ Source/RimNauts2/RimNauts2/Map/MapGen/TerrainPatch.cs
@@ -3,14 +3,14 @@ using Verse;
 
 namespace RimNauts2 {
     [HarmonyPatch(typeof(RimWorld.GenStep_Terrain), "Generate")]
-    static class TerrainPatch {
-        static bool Prefix(Map map, GenStepParams parms) {
+    public static class TerrainPatch {
+        public static bool Prefix(Map map, GenStepParams parms) {
             // check if it's our biome. If not, skip the patch
             if (map.Biome.defName != "RockMoonBiome") {
                 return true;
             }
 
-            (new GenStep_MoonTerrain()).Generate(map, parms);
+            new GenStep_MoonTerrain().Generate(map, parms);
             return false;
         }
     }
