5ae215a566cf2916a63a67431f2c4249a3f6e764
Sindre Eiklid <sindre0830@gmail.com>
2022-10-17 00:02:00 +0200

    Add back transport from/to files

---
+++ Source/RimNauts2/RimNauts2/World/TransportpodFromSatellite.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace RimNauts2 {
+    [HarmonyPatch(typeof(RimWorld.Planet.TravelingTransportPods))]
+    [HarmonyPatch("Start", MethodType.Getter)]
+    public static class TransportpodFromSatellite {
+        [HarmonyPostfix]
+        public static void StartAtShip(RimWorld.Planet.TravelingTransportPods __instance, ref Vector3 __result) {
+            int num = (int) typeof(RimWorld.Planet.TravelingTransportPods).GetField("initialTile", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+            foreach (RimWorld.Planet.WorldObject worldObject in from o in Find.World.worldObjects.AllWorldObjects
+                                                                where o is Satellite
+                                                                select o) {
+                if (worldObject.Tile == num) __result = worldObject.DrawPos;
+            }
+        }
+    }
+}
