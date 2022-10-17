053dc5b6e21309db982c28d4c4935f4b795280da
Sindre Eiklid <sindre0830@gmail.com>
2022-10-17 18:31:40 +0200

    Fix biome tile on world

---
+++ Source/RimNauts2/RimNauts2/Biome/SetSatelliteBiome.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace RimNauts2 {
+    [HarmonyPatch(typeof(RimWorld.Planet.WorldGenStep_Terrain), "BiomeFrom")]
+    internal static class SetSatelliteBiome {
+        public static int i = 0;
+        internal static void Postfix(RimWorld.Planet.Tile ws, int tileID, ref RimWorld.BiomeDef __result) {
+            if (i < Generate_Satellites.total_satellite_amount && __result == RimWorld.BiomeDefOf.Ocean) {
+                __result = BiomeDefOf.SatelliteBiome;
+                i++;
+            }
+        }
+    }
+}
---
+++ Source/RimNauts2/RimNauts2/World/ChangeBiomeMaterial.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+
+namespace RimNauts2.World {
+    [HarmonyPatch(typeof(RimWorld.BiomeDef), nameof(RimWorld.BiomeDef.DrawMaterial), MethodType.Getter)]
+    public static class ChangeBiomeMaterial {
+        public static void Prefix(ref RimWorld.BiomeDef __instance) {
+            if (__instance == BiomeDefOf.SatelliteBiome || __instance == BiomeDefOf.RockMoonBiome) {
+                __instance = RimWorld.BiomeDefOf.Ocean;
+            }
+        }
+    }
+}
