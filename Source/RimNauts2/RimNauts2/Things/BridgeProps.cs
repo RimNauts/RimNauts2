603614542c4126caa0a7b45536a53bba1d112e2c
Sindre Eiklid <sindre0830@gmail.com>
2022-10-27 15:25:33 +0200

    Add steel bridge

--- Source/RimNauts2/RimNauts2/Things/BridgeProps.cs
+++ Source/RimNauts2/RimNauts2/Things/BridgeProps.cs
@@ -3,9 +3,13 @@ using Verse;
 
 namespace RimNauts2 {
     [HarmonyPatch(typeof(RimWorld.SectionLayer_BridgeProps), "ShouldDrawPropsBelow")]
-    class BridgeProps {
+    class SectionLayer_BridgeProps_ShouldDrawPropsBelow {
         public static bool Prefix(IntVec3 c, TerrainGrid terrGrid, ref bool __result) {
             if (terrGrid.TerrainAt(c).defName == "RimNauts2_Vacuum_Platform") {
+                __result = false;
+                return false;
+            } else if (terrGrid.TerrainAt(c).defName == "RimNauts2_SteelBridge") {
+                // should patch RimWorld.SectionLayer_BridgeProps.Regenerate to draw custom prop
                 __result = false;
                 return false;
             } else return true;
