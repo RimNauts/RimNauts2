796900c35acfb35eb188763726db3471b71bf465
Sindre Eiklid <sindre0830@gmail.com>
2022-10-27 13:47:07 +0200

    Fix ore spawning

--- Source/RimNauts2/RimNauts2/World/Transport.cs
+++ Source/RimNauts2/RimNauts2/World/Transport.cs
@@ -96,8 +96,8 @@ namespace RimNauts2 {
     [HarmonyPatch(typeof(RimWorld.Planet.WorldGrid), nameof(RimWorld.Planet.WorldGrid.TraversalDistanceBetween))]
     public static class TransportpodSatelliteIgnoreMaxRange {
         public static void Postfix(int start, int end, bool passImpassable, int maxDist, ref int __result) {
-            bool to_moon = Find.World.grid.tiles.ElementAt(end).biome.defName == "RimNauts2_Satellite_Biome";
-            bool from_moon = Find.World.grid.tiles.ElementAt(start).biome.defName == "RimNauts2_Satellite_Biome";
+            bool to_moon = Find.World.grid.tiles.ElementAt(end).biome.defName.Contains("RimNauts2");
+            bool from_moon = Find.World.grid.tiles.ElementAt(start).biome.defName.Contains("RimNauts2");
             if (to_moon || from_moon) {
                 if (!from_moon) {
                     if (Find.WorldObjects.WorldObjectAt<Satellite>(end).def.defName.Contains("Ore")) {
@@ -113,25 +113,28 @@ namespace RimNauts2 {
 
     [HarmonyPatch(typeof(RimWorld.Planet.TravelingTransportPods), "Start", MethodType.Getter)]
     public static class TransportpodFromSatelliteAnimation {
-        public static void Postfix(RimWorld.Planet.TravelingTransportPods __instance, ref Vector3 __result) {
-            int num = (int) typeof(RimWorld.Planet.TravelingTransportPods).GetField("initialTile", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
-            foreach (RimWorld.Planet.WorldObject worldObject in from o in Find.World.worldObjects.AllWorldObjects
-                                                                where o is Satellite
-                                                                select o) {
-                if (worldObject.Tile == num) __result = worldObject.DrawPos;
+        public static bool Prefix(RimWorld.Planet.TravelingTransportPods __instance, ref Vector3 __result) {
+            int initialTile = (int) typeof(RimWorld.Planet.TravelingTransportPods).GetField("initialTile", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+            foreach (RimWorld.Planet.WorldObject worldObject in Find.World.worldObjects.AllWorldObjects) {
+                if (worldObject.Tile == initialTile) {
+                    __result = worldObject.DrawPos;
+                    return false;
+                }
             }
+            return true;
         }
     }
 
     [HarmonyPatch(typeof(RimWorld.Planet.TravelingTransportPods), "End", MethodType.Getter)]
     public static class TransportpodToSatelliteAnimation {
-        public static void Postfix(RimWorld.Planet.TravelingTransportPods __instance, ref Vector3 __result) {
-            int destinationTile = __instance.destinationTile;
-            foreach (RimWorld.Planet.WorldObject worldObject in from o in Find.World.worldObjects.AllWorldObjects
-                                                                where o is Satellite
-                                                                select o) {
-                if (worldObject.Tile == destinationTile) __result = worldObject.DrawPos;
+        public static bool Prefix(RimWorld.Planet.TravelingTransportPods __instance, ref Vector3 __result) {
+            foreach (RimWorld.Planet.WorldObject worldObject in Find.World.worldObjects.AllWorldObjects) {
+                if (worldObject.Tile == __instance.destinationTile) {
+                    __result = worldObject.DrawPos;
+                    return false;
+                }
             }
+            return true;
         }
     }
 }
