0cd764590f3cb5a0b4609615ba68934b6133f314
Sindre Eiklid <sindre0830@gmail.com>
2022-10-27 14:21:46 +0200

    Add stripes genstep

---
+++ Source/RimNauts2/RimNauts2/Map/MoonStripped/Stripes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Verse;
+
+namespace RimNauts2.MoonStripped {
+    class Stripes : GenStep {
+        public override int SeedPart => 262606459;
+
+        public override void Generate(Map map, GenStepParams parms) {
+            int radius = (map.Size.x / 2) - 3;
+            for (int ind = 1; ind <= Rand.RangeInclusive(4, 10); ind++) {
+                int dz = (int) ((Rand.Value - 0.5) * map.Size.z);
+                for (int dx = 1; dx <= map.Size.x - 1; dx++) {
+                    int set = (int) (-22f * Math.Sin((3.14f / map.Size.x) * dx - 0) + map.Center.z + dz * 0.75);
+                    wide_brush(new IntVec3(dx, 0, set), "Slate", map, Rand.RangeInclusive(1, 4));
+                }
+            }
+        }
+
+        public void wide_brush(IntVec3 pos, string defName, Map map, int halfWidth) {
+            for (int dz = -halfWidth; dz <= halfWidth; dz++) {
+                map.terrainGrid.SetTerrain(new IntVec3(pos.x, 0, pos.z + dz), DefDatabase<ThingDef>.GetNamed(defName).building.naturalTerrain);
+            }
+        }
+    }
+}
