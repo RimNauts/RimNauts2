0e455d2c087a5a4b488b6b22f8073f015c2ae542
Sindre Eiklid <sindre0830@gmail.com>
2022-10-28 09:44:37 +0200

    Move text to xml for translation

--- Source/RimNauts2/RimNauts2/Things/SatelliteDish.cs
+++ Source/RimNauts2/RimNauts2/Things/SatelliteDish.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 namespace RimNauts2 {
     public class CompProperties_SatelliteDish : CompProperties {
         public string worldObject;
+        public string label;
+        public string desc;
+        public string failMessage;
+        public string successMessage;
         public string texPath;
         public CompProperties_SatelliteDish() => compClass = typeof(SatelliteDish);
     }
@@ -13,18 +17,18 @@ namespace RimNauts2 {
         public CompProperties_SatelliteDish Props => (CompProperties_SatelliteDish) props;
 
         public override IEnumerable<Gizmo> CompGetGizmosExtra() {
-            string label = "Look for a " + Props.worldObject.Substring(14).ToLower() + " moon";
+            string label = Props.label;
             if (Prefs.DevMode) label += " (Dev)";
             Command_Action cmd = new Command_Action {
                 defaultLabel = label,
-                defaultDesc = "Your pawn will look for a " + Props.worldObject.Substring(14).ToLower() + " moon orbiting the planet.",
+                defaultDesc = Props.desc,
                 icon = ContentFinder<Texture2D>.Get(Props.texPath, true),
                 action = new Action(action)
             };
             if (!Prefs.DevMode) {
                 if (SatelliteContainer.size(Satellite_Type.Artifical_Satellite) < (SatelliteContainer.size(Satellite_Type.Moon) + 1) * 2) {
                     int diff = (SatelliteContainer.size(Satellite_Type.Moon) + 1) * 2 - SatelliteContainer.size(Satellite_Type.Artifical_Satellite);
-                    cmd.Disable("Requires " + diff.ToString() + " more satellite" + (diff > 1 ? "s" : "") + " orbiting the planet.");
+                    cmd.Disable(diff.ToString() + " " + Props.failMessage);
                 }
             }
             yield return cmd;
@@ -41,7 +45,7 @@ namespace RimNauts2 {
 
             if (new_moon_tile_id != -1) {
                 Generate_Satellites.add_satellite(new_moon_tile_id, Satellite_Type.Moon, def_name: Props.worldObject);
-                Messages.Message("Successfully found a " + Props.worldObject.Substring(14).ToLower() + " moon orbiting the planet!", RimWorld.MessageTypeDefOf.PositiveEvent, true);
+                Messages.Message(Props.successMessage, RimWorld.MessageTypeDefOf.PositiveEvent, true);
             } else {
                 Log.Error("RimNauts2: Couldn't find a free tile to spawn a moon on. Either the map size is too small to spawn all the satellites or increase the total satellite objects in settings (requires a new save).");
             }
