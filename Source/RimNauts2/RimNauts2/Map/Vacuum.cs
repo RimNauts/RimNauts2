82846cd622641af8909dac591ac1fab953277bbf
Sindre Eiklid <sindre0830@gmail.com>
2022-10-29 18:31:40 +0200

    Add vacuum genstep

--- Source/RimNauts2/RimNauts2/Map/Vacuum.cs
+++ Source/RimNauts2/RimNauts2/Map/Vacuum.cs
@@ -1,12 +1,14 @@
 ﻿using Verse;
 
-namespace RimNauts2.SpaceStation {
-    class Terrain : GenStep {
+namespace RimNauts2 {
+    class Vacuum : GenStep {
         public override int SeedPart => 262606459;
 
         public override void Generate(Map map, GenStepParams parms) {
             foreach (IntVec3 current in map.AllCells) {
                 map.terrainGrid.SetTerrain(current, DefDatabase<TerrainDef>.GetNamed("RimNauts2_Vacuum"));
+                MapGenerator.Elevation[current] = 0.0f;
+                MapGenerator.Fertility[current] = 0.0f;
             }
             MapGenerator.PlayerStartSpot = new IntVec3(1, 0, 1);
         }
