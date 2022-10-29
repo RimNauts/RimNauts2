5a88b954ccaddbe4a94eac7b936bb18b00a66240
Sindre Eiklid <sindre0830@gmail.com>
2022-10-29 23:13:55 +0200

    Refactor asteroid genstep and change def structure

--- Source/RimNauts2/RimNauts2/Map/AsteroidOre/RocksFromGrid.cs
+++ Source/RimNauts2/RimNauts2/Map/AsteroidOre/RocksFromGrid.cs
@@ -1,60 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using Verse;
+﻿using Verse;
+using UnityEngine;
 
 namespace RimNauts2.AsteroidOre {
     class RocksFromGrid : GenStep {
         public string rock_def_name = "Limestone";
         public string ore_def_name = "MineableSteel";
+        public Vector2 ore_chance_outer = new Vector3(0.30f, 0.50f);
+        public Vector2 ore_chance_inner = new Vector3(0.50f, 0.70f);
 
         public override int SeedPart => 262606459;
 
         public override void Generate(Map map, GenStepParams parms) {
-            map.regionAndRoomUpdater.Enabled = false;
-            float num = 0.7f;
-            List<RoofThreshold> list = new List<RoofThreshold> {
-                new RoofThreshold {
-                    roofDef = RimWorld.RoofDefOf.RoofRockThick,
-                    minGridVal = num * 1.14f
-                },
-                new RoofThreshold {
-                    roofDef = RimWorld.RoofDefOf.RoofRockThin,
-                    minGridVal = num * 1.04f
-                }
-            };
-            MapGenFloatGrid elevation = MapGenerator.Elevation;
-            MapGenFloatGrid caves = MapGenerator.Caves;
-            MapGenFloatGrid fertility = MapGenerator.Fertility;
+            float outer_chance = Rand.Range(ore_chance_outer.x, ore_chance_outer.y);
+            float inner_chance = Rand.Range(ore_chance_inner.x, ore_chance_inner.y);
             foreach (IntVec3 current in map.AllCells) {
-                float num2 = elevation[current];
-                if (num2 > num) {
-                    if (caves[current] <= 0f) {
-                        ThingDef def = rock_at(fertility[current], rock_def_name);
-                        if (def == ThingDef.Named(rock_def_name)) {
-                            GenSpawn.Spawn(DefDatabase<ThingDef>.GetNamed(Rand.Value > 0.60 ? rock_def_name : ore_def_name, true), current, map);
-                        } else continue;
+                if (MapGenerator.Fertility[current] <= 0.1f) {
+                    if (MapGenerator.Elevation[current] > 0.7f && MapGenerator.Caves[current] <= 0.0f) {
+                        GenSpawn.Spawn(DefDatabase<ThingDef>.GetNamed(Rand.Value > inner_chance ? rock_def_name : ore_def_name, true), current, map);
+                    } else {
+                        GenSpawn.Spawn(DefDatabase<ThingDef>.GetNamed(Rand.Value > outer_chance ? rock_def_name : ore_def_name, true), current, map);
                     }
-                    for (int i = 0; i < list.Count; i++) {
-                        if (num2 > list[i].minGridVal) {
-                            map.roofGrid.SetRoof(current, list[i].roofDef);
-                            break;
-                        }
-                    }
+                    map.terrainGrid.SetTerrain(current, ThingDef.Named(rock_def_name).building.naturalTerrain);
                 }
             }
-            MapGenerator.PlayerStartSpot = new IntVec3(1, 0, 1);
-        }
-
-        public static ThingDef rock_at(float fertility, string rock_def_name) {
-            ThingDef thingDef = ThingDef.Named(rock_def_name);
-            // Changes the ratio of rock types
-            if (fertility > 0.1f) thingDef = ThingDef.Named("Slate");
-            return thingDef;
-        }
-
-        private class RoofThreshold {
-            public RoofDef roofDef;
-            public float minGridVal;
         }
     }
 }
