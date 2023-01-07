2c8b3c5a4713db996ff717bb5e3568aab784a390
Sindre Eiklid <sindre0830@gmail.com>
2023-01-07 23:10:28 +0100

    Add info logging on startup #22

--- Source/Universum/Universum/Universum.cs
+++ Source/Universum/Universum/Universum.cs
@@ -5,6 +5,8 @@ namespace Universum {
     public static class Universum {
         static Universum() {
             new HarmonyLib.Harmony("sindre0830.universum").PatchAll(Assembly.GetExecutingAssembly());
+
+            Verse.Log.Message(Verse.TranslatorFormattedStringExtensions.Translate("Info_ModLoaded", new Verse.NamedArgument[] { Info.name, Info.version }));
         }
     }
 }
