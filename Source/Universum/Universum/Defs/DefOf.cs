82adfa9361326fba022dd49b67401fde2e465939
Sindre Eiklid <sindre0830@gmail.com>
2023-01-07 22:16:01 +0100

    Fix ObjectsDef naming #22

--- Source/Universum/Universum/Defs/DefOf.cs
+++ Source/Universum/Universum/Defs/DefOf.cs
@@ -3,8 +3,6 @@
     public static class DefOf {
         public static ObjectsDef Objects;
 
-        static DefOf() {
-            RimWorld.DefOfHelper.EnsureInitializedInCtor(typeof(ObjectsDef));
-        }
+        static DefOf() => RimWorld.DefOfHelper.EnsureInitializedInCtor(typeof(DefOf));
     }
 }
