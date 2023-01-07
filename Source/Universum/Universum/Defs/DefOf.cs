651618ea2a1d2c837a7cf68cd4e6956169d774ab
Sindre Eiklid <sindre0830@gmail.com>
2023-01-07 21:14:10 +0100

    Add Objects def loader #22

---
+++ Source/Universum/Universum/Defs/DefOf.cs
@@ -0,0 +1,10 @@
+﻿namespace Universum {
+    [RimWorld.DefOf]
+    public static class DefOf {
+        public static ObjectsDef Objects;
+
+        static DefOf() {
+            RimWorld.DefOfHelper.EnsureInitializedInCtor(typeof(ObjectsDef));
+        }
+    }
+}
---
+++ Source/Universum/Universum/Defs/ObjectsDef.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace Universum {
+    public class ObjectsDef : Verse.Def {
+        public List<string> Utilities;
+    }
+}
