d45a111c79333c4a7f8f6795528793bb7564d57f
Sindre Eiklid <sindre0830@gmail.com>
2023-01-10 14:00:29 +0100

    Add biome properties class to get mod extension info

---
+++ Source/Universum/Universum/Utilities/Biome/Properties.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Universum.Utilities.Biome {
+    public class Properties : Verse.DefModExtension {
+        public List<string> allowed_utilities = new List<string>();
+        private static readonly Dictionary<string, Properties> DefaultsByPackageId = new Dictionary<string, Properties>();
+        private static readonly Dictionary<string, Properties> DefaultsByDefName = new Dictionary<string, Properties>();
+
+        public static Properties[] GetAll() {
+            Properties[] biomePropertiesArray = new Properties[Verse.DefDatabase<RimWorld.BiomeDef>.DefCount];
+            foreach (RimWorld.BiomeDef biome in Verse.DefDatabase<RimWorld.BiomeDef>.AllDefs) {
+                Properties biomeProperties = ((biome.GetModExtension<Properties>() ?? DefaultsByDefName.TryGetValue(biome.defName)) ?? DefaultsByPackageId.TryGetValue(biome.modContentPack.ModMetaData.PackageIdNonUnique)) ?? new Properties();
+                biomePropertiesArray[biome.index] = biomeProperties;
+            }
+            return biomePropertiesArray;
+        }
+    }
+}
