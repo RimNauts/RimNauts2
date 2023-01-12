0b8a31721300630249d444dce25a289dba32ebb7
Sindre Eiklid <sindre0830@gmail.com>
2023-01-12 13:11:06 +0100

    Add ocean masking utility #22

---
+++ Source/Universum/Universum/Utilities/Ocean_Masking.cs
@@ -0,0 +1,11 @@
+﻿namespace Universum.Utilities {
+    /**
+     * Masks biome tile as ocean to apply ocean material when drawing planet.
+     */
+    [HarmonyLib.HarmonyPatch(typeof(RimWorld.BiomeDef), "DrawMaterial", HarmonyLib.MethodType.Getter)]
+    public static class BiomeDef_DrawMaterial {
+        public static void Prefix(ref RimWorld.BiomeDef __instance) {
+            if (Cache.allowed_utility(__instance, "(Universum) Ocean Masking")) __instance = RimWorld.BiomeDefOf.Ocean;
+        }
+    }
+}
