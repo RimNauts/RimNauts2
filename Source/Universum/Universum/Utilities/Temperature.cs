12ce659fdaf1b29978dba305a2db0e2cb64ffebf
Sindre Eiklid <sindre0830@gmail.com>
2023-01-15 16:06:58 +0100

    Reduce temperature change in space by 99%

--- Source/Universum/Universum/Utilities/Temperature.cs
+++ Source/Universum/Universum/Utilities/Temperature.cs
@@ -1,4 +1,6 @@
-﻿namespace Universum.Utilities {
+﻿using System.Reflection;
+
+namespace Universum.Utilities {
     [HarmonyLib.HarmonyPatch(typeof(Verse.MapTemperature), "OutdoorTemp", HarmonyLib.MethodType.Getter)]
     public static class MapTemperature_OutdoorTemp {
         public static void Postfix(ref float __result, Verse.Map ___map) {
@@ -10,6 +12,14 @@
     public static class MapTemperature_SeasonalTemp {
         public static void Postfix(ref float __result, Verse.Map ___map) {
             if (Cache.allowed_utility(___map, "(Universum) Temperature")) __result = Cache.temperature(___map);
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(Verse.RoomTempTracker), "WallEqualizationTempChangePerInterval")]
+    public static class RoomTempTracker_WallEqualizationTempChangePerInterval {
+        public static void Postfix(ref float __result, Verse.RoomTempTracker __instance) {
+            Verse.Room room = (Verse.Room) typeof(Verse.RoomTempTracker).GetField("room", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(__instance);
+            if (Cache.allowed_utility(room.Map, "(Universum) Temperature")) __result *= 0.01f;
         }
     }
 }
