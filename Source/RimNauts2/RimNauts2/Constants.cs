54178c74a527d9cee38a161c978c6549c38e7755
Sindre Eiklid <sindre0830@gmail.com>
2023-01-17 22:20:53 +0100

    Add constants from Universum to RimNauts

---
+++ Source/RimNauts2/RimNauts2/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace RimNauts2 {
+    public static class Info {
+        public static readonly string name = "RimNauts 2";
+        public static readonly string version = "2.3.1";
+    }
+
+    public static class Style {
+        public static readonly string name_prefix = Info.name + ": ";
+        public static readonly string tab = "        ";
+    }
+}
