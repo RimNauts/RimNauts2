dce71f855399de5628eb5a98268a5ffbce16b1f0
Sindre Eiklid <sindre0830@gmail.com>
2023-01-17 22:53:44 +0100

    Print RimNauts mod info

--- Source/RimNauts2/RimNauts2/RimNauts2.cs
+++ Source/RimNauts2/RimNauts2/RimNauts2.cs
@@ -5,6 +5,12 @@ namespace RimNauts2 {
     public static class RimNauts2 {
         static RimNauts2() {
             new HarmonyLib.Harmony("sindre0830.RimNauts2").PatchAll(Assembly.GetExecutingAssembly());
+            // print mod info
+            Logger.print(
+                Logger.Importance.Info,
+                key: "RimNauts.Info.mod_loaded",
+                args: new Verse.NamedArgument[] { Info.name, Info.version }
+            );
         }
     }
 }
