5c68f25a6efe68933903b87bc648b8b9a63a25ae
Sindre Eiklid <sindre0830@gmail.com>
2023-01-17 12:39:15 +0100

    Add utility metadata and refactor IDs #22

--- Source/Universum/Universum/Utilities/Ocean_Masking.cs
+++ Source/Universum/Universum/Utilities/Ocean_Masking.cs
@@ -7,7 +7,7 @@ namespace Universum.Utilities {
     [HarmonyLib.HarmonyPatch(typeof(RimWorld.BiomeDef), "DrawMaterial", HarmonyLib.MethodType.Getter)]
     public static class BiomeDef_DrawMaterial {
         public static void Prefix(ref RimWorld.BiomeDef __instance) {
-            if (Cache.allowed_utility(__instance, "(Universum) Ocean Masking")) __instance = RimWorld.BiomeDefOf.Ocean;
+            if (Cache.allowed_utility(__instance, "Universum.ocean_masking")) __instance = RimWorld.BiomeDefOf.Ocean;
         }
     }
 
@@ -19,7 +19,7 @@ namespace Universum.Utilities {
     public static class WorldLayer_CurrentMapTile_Tile {
         public static void Postfix(ref int __result) {
             if (__result == -1) return;
-            if (Cache.allowed_utility(Verse.Find.World.grid.tiles.ElementAt(__result).biome, "(Universum) Ocean Masking")) __result = -1;
+            if (Cache.allowed_utility(Verse.Find.World.grid.tiles.ElementAt(__result).biome, "Universum.ocean_masking")) __result = -1;
         }
     }
 }
