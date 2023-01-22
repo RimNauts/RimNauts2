31c68e838f280f456cf258ffb96567e09fb4d246
Sindre Eiklid <sindre0830@gmail.com>
2023-01-22 15:20:54 +0100

    Add stat printing option in debug actions

--- Changelog.md
+++ Changelog.md
@@ -1,2 +1,2 @@
 # Changelog
-- Update version to print the correct version on startup (was 3.1.0  at update 3.1.2)
+- Add stat printing in debug actions
--- Source/RimNauts2/RimNauts2/Settings/DebugToolsSatellites.cs
+++ Source/RimNauts2/RimNauts2/Settings/DebugToolsSatellites.cs
@@ -1,10 +1,22 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using Verse;
 
 namespace RimNauts2 {
     public static class DebugToolsSatellites {
         [DebugAction("Spawning", "RimNauts2: Regenerate objects", false, false, false, 0, false, actionType = DebugActionType.Action, allowedGameStates = AllowedGameStates.PlayingOnWorld)]
         public static void RegenerateSatellites() {
             Generate_Satellites.regenerate_satellites();
+        }
+        [DebugAction("Spawning", "RimNauts2: Stats", false, false, false, 0, false, actionType = DebugActionType.Action, allowedGameStates = AllowedGameStates.PlayingOnWorld)]
+        public static void PrintStats() {
+            int asteroid_ore = 0;
+            int crashing_asteroid = 0;
+            foreach (KeyValuePair<int, Satellite> satellite in RimNauts_GameComponent.satellites) {
+                if (satellite.Value.mineral_rich) asteroid_ore++;
+                if (satellite.Value.can_out_of_bounds) crashing_asteroid++;
+            }
+            Verse.Log.Message("RimNauts.mineral_rich_asteroids".Translate() + ": " + asteroid_ore.ToString());
+            Verse.Log.Message("RimNauts.crashing_asteroids".Translate() + ": " + crashing_asteroid.ToString());
         }
     }
 }
