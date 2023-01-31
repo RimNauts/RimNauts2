76e476cd79ea6e43c21b7032b041ec4c9f708a3d
Sindre Eiklid <sindre0830@gmail.com>
2023-01-31 19:04:06 +0100

    Fix hitboxes of large launchpad

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,4 @@
 # Changelog
 - Update utility IDs to match the latest version of Universum
 - Fix SOS2 weather so it stays on space maps
+- Fix hitboxes of large launchpad
---
+++ Source/RimNauts2/RimNauts2/Things/Pod_Launcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace RimNauts2 {
+    [HarmonyLib.HarmonyPatch(typeof(RimWorld.FuelingPortUtility), "GetFuelingPortCell", new Type[] { typeof(Building) })]
+    public static class FuelingPortUtility_GetFuelingPortCell {
+        public static bool Prefix(Building podLauncher, ref IntVec3 __result) {
+            if (podLauncher.def.defName != "RimNauts2_PodLauncher") return true;
+            __result = podLauncher.Position;
+            return false;
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(RimWorld.PlaceWorker_FuelingPort), "DrawGhost")]
+    public static class PlaceWorker_FuelingPort_DrawGhost {
+        public static bool Prefix(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null) {
+            if (def.defName != "RimNauts2_PodLauncher") return true;
+            Map currentMap = Find.CurrentMap;
+            rot = Rot4.South;
+            center.x++;
+            if (def.building == null || !def.building.hasFuelingPort || !RimWorld.FuelingPortUtility.GetFuelingPortCell(center, rot).Standable(currentMap)) return false;
+            RimWorld.PlaceWorker_FuelingPort.DrawFuelingPortCell(center, rot);
+            return false;
+        }
+    }
+}
