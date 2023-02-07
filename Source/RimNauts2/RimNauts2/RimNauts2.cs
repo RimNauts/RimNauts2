ccca44d0fc673d63eb379c1c52df68597655c75b
Sindre Eiklid <sindre0830@gmail.com>
2023-02-07 11:48:24 +0100

    Unpatch PawnGroupMakerUtility if Rim War is present

--- Source/RimNauts2/RimNauts2/RimNauts2.cs
+++ Source/RimNauts2/RimNauts2/RimNauts2.cs
@@ -1,15 +1,18 @@
 ﻿using System.Reflection;
+using Verse;
 
 namespace RimNauts2 {
     [Verse.StaticConstructorOnStartup]
     public static class RimNauts2 {
         static RimNauts2() {
-            new HarmonyLib.Harmony("sindre0830.rimnauts2").PatchAll(Assembly.GetExecutingAssembly());
+            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("sindre0830.rimnauts2");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            if (ModsConfig.IsActive("torann.rimwar")) harmony.Unpatch(typeof(RimWorld.PawnGroupMakerUtility).GetMethod("GeneratePawns"), typeof(PawnGroupMakerUtility_GeneratePawns).GetMethod("Postfix"));
             // print mod info
             Logger.print(
                 Logger.Importance.Info,
                 key: "RimNauts.Info.mod_loaded",
-                args: new Verse.NamedArgument[] { Info.name, Info.version }
+                args: new NamedArgument[] { Info.name, Info.version }
             );
         }
     }
