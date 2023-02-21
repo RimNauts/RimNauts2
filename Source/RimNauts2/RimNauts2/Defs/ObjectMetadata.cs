ff994925445451cb8ee6e940bcfbed1362539dae
Sindre Eiklid <sindre0830@gmail.com>
2023-02-21 19:57:39 +0100

    Move object metadata to def

---
+++ Source/RimNauts2/RimNauts2/Defs/ObjectMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace RimNauts2.Defs {
+    public class ObjectMetadata : Def {
+        public List<string> texture_paths = new List<string>();
+        public Vector3 orbit_position = Vector3.one;
+        public Vector3 orbit_spread = Vector3.one;
+        public Vector2 orbit_speed_between = Vector2.one;
+        public Vector2 size_between = Vector2.one;
+        public Vector2 color_between = Vector2.one;
+        public bool random_rotation;
+        public bool random_direction;
+    }
+}
--- Source/RimNauts2/RimNauts2/World/Type.cs
+++ Source/RimNauts2/RimNauts2/World/Type.cs
@@ -14,6 +14,23 @@ namespace RimNauts2.World {
     }
 
     public static class TypeExtension {
+        public static Defs.ObjectMetadata asteroid_defs = (Defs.ObjectMetadata) GenDefDatabase.GetDef(
+            typeof(Defs.ObjectMetadata),
+            "RimNauts2_Object_Metadata_Asteroid"
+        );
+        public static Defs.ObjectMetadata moon_defs = (Defs.ObjectMetadata) GenDefDatabase.GetDef(
+            typeof(Defs.ObjectMetadata),
+            "RimNauts2_Object_Metadata_Moon"
+        );
+        public static Defs.ObjectMetadata satellite_defs = (Defs.ObjectMetadata) GenDefDatabase.GetDef(
+            typeof(Defs.ObjectMetadata),
+            "RimNauts2_Object_Metadata_Satellite"
+        );
+        public static Defs.ObjectMetadata space_station_defs = (Defs.ObjectMetadata) GenDefDatabase.GetDef(
+            typeof(Defs.ObjectMetadata),
+            "RimNauts2_Object_Metadata_SpaceStation"
+        );
+
         public static Objects.NEO neo(this Type type) {
             switch (type) {
                 case Type.Asteroid:
@@ -30,6 +47,7 @@ namespace RimNauts2.World {
                     return null;
             }
         }
+
         public static Objects.NEO neo(
             this Type type,
             string texture_path,
@@ -114,58 +132,32 @@ namespace RimNauts2.World {
             }
         }
 
-        public static string texture_path(this Type type) {
+        public static Defs.ObjectMetadata defs(this Type type) {
             switch (type) {
                 case Type.Asteroid:
-                    return Objects.AsteroidConfig.texture_paths.RandomElement();
+                    return asteroid_defs;
                 case Type.AsteroidOre:
-                    return Objects.AsteroidConfig.texture_paths.RandomElement();
+                    return asteroid_defs;
                 case Type.AsteroidCrashing:
-                    return Objects.AsteroidConfig.texture_paths.RandomElement();
+                    return asteroid_defs;
                 case Type.Moon:
-                    return Objects.MoonConfig.texture_paths.RandomElement();
+                    return moon_defs;
                 case Type.Satellite:
-                    return Objects.SatelliteConfig.texture_paths.RandomElement();
+                    return satellite_defs;
                 case Type.SpaceStation:
-                    return Objects.SpaceStationConfig.texture_paths.RandomElement();
+                    return space_station_defs;
                 default:
-                    return Objects.AsteroidConfig.texture_paths[0];
+                    return asteroid_defs;
             }
         }
 
+        public static string texture_path(this Type type) {
+            return type.defs().texture_paths.RandomElement();
+        }
+
         public static Vector3 orbit_position(this Type type) {
-            Vector3 orbit_position;
-            Vector3 orbit_spread;
-            switch (type) {
-                case Type.Asteroid:
-                    orbit_position = Objects.AsteroidConfig.orbit_position;
-                    orbit_spread = Objects.AsteroidConfig.orbit_spread;
-                    break;
-                case Type.AsteroidOre:
-                    orbit_position = Objects.AsteroidConfig.orbit_position;
-                    orbit_spread = Objects.AsteroidConfig.orbit_spread;
-                    break;
-                case Type.AsteroidCrashing:
-                    orbit_position = Objects.AsteroidConfig.orbit_position;
-                    orbit_spread = Objects.AsteroidConfig.orbit_spread;
-                    break;
-                case Type.Moon:
-                    orbit_position = Objects.MoonConfig.orbit_position;
-                    orbit_spread = Objects.MoonConfig.orbit_spread;
-                    break;
-                case Type.Satellite:
-                    orbit_position = Objects.SatelliteConfig.orbit_position;
-                    orbit_spread = Objects.SatelliteConfig.orbit_spread;
-                    break;
-                case Type.SpaceStation:
-                    orbit_position = Objects.SpaceStationConfig.orbit_position;
-                    orbit_spread = Objects.SpaceStationConfig.orbit_spread;
-                    break;
-                default:
-                    orbit_position = new Vector3(250.0f, 0.0f, 250.0f);
-                    orbit_spread = new Vector3(0.0f, 0.0f, 0.0f);
-                    break;
-            }
+            Vector3 orbit_position = type.defs().orbit_position;
+            Vector3 orbit_spread = type.defs().orbit_spread;
             return new Vector3 {
                 x = orbit_position.x + (float) ((Rand.Value - 0.5f) * (orbit_position.x * orbit_spread.x)),
                 y = Rand.Range(Math.Abs(orbit_position.y) * -1, Math.Abs(orbit_position.y)),
@@ -173,163 +165,27 @@ namespace RimNauts2.World {
             };
         }
 
-        public static Vector3 orbit_spread(this Type type) {
-            switch (type) {
-                case Type.Asteroid:
-                    return Objects.AsteroidConfig.orbit_spread;
-                case Type.AsteroidOre:
-                    return Objects.AsteroidConfig.orbit_spread;
-                case Type.AsteroidCrashing:
-                    return Objects.AsteroidConfig.orbit_spread;
-                case Type.Moon:
-                    return Objects.MoonConfig.orbit_spread;
-                case Type.Satellite:
-                    return Objects.SatelliteConfig.orbit_spread;
-                case Type.SpaceStation:
-                    return Objects.SpaceStationConfig.orbit_spread;
-                default:
-                    return new Vector3(0.0f, 0.0f, 0.0f);
-            }
-        }
-
         public static float orbit_speed(this Type type) {
-            Vector2 speed_between;
-            switch (type) {
-                case Type.Asteroid:
-                    speed_between = Objects.AsteroidConfig.orbit_speed_between;
-                    break;
-                case Type.AsteroidOre:
-                    speed_between = Objects.AsteroidConfig.orbit_speed_between;
-                    break;
-                case Type.AsteroidCrashing:
-                    speed_between = Objects.AsteroidConfig.orbit_speed_between;
-                    break;
-                case Type.Moon:
-                    speed_between = Objects.MoonConfig.orbit_speed_between;
-                    break;
-                case Type.Satellite:
-                    speed_between = Objects.SatelliteConfig.orbit_speed_between;
-                    break;
-                case Type.SpaceStation:
-                    speed_between = Objects.SpaceStationConfig.orbit_speed_between;
-                    break;
-                default:
-                    speed_between = new Vector2(1.0f, 1.0f);
-                    break;
-            }
+            Vector2 speed_between = type.defs().orbit_speed_between;
             return Rand.Range(speed_between.x, speed_between.y);
         }
 
         public static float size(this Type type) {
-            Vector2 size_between;
-            switch (type) {
-                case Type.Asteroid:
-                    size_between = Objects.AsteroidConfig.size_between;
-                    break;
-                case Type.AsteroidOre:
-                    size_between = Objects.AsteroidConfig.size_between;
-                    break;
-                case Type.AsteroidCrashing:
-                    size_between = Objects.AsteroidConfig.size_between;
-                    break;
-                case Type.Moon:
-                    size_between = Objects.MoonConfig.size_between;
-                    break;
-                case Type.Satellite:
-                    size_between = Objects.SatelliteConfig.size_between;
-                    break;
-                case Type.SpaceStation:
-                    size_between = Objects.SpaceStationConfig.size_between;
-                    break;
-                default:
-                    size_between = new Vector2(1.0f, 1.0f);
-                    break;
-            }
+            Vector2 size_between = type.defs().size_between;
             return Rand.Range(size_between.x, size_between.y) * 20 * Find.WorldGrid.averageTileSize;
         }
 
         public static float color(this Type type) {
-            Vector2 color_between;
-            switch (type) {
-                case Type.Asteroid:
-                    color_between = Objects.AsteroidConfig.color_between;
-                    break;
-                case Type.AsteroidOre:
-                    color_between = Objects.AsteroidConfig.color_between;
-                    break;
-                case Type.AsteroidCrashing:
-                    color_between = Objects.AsteroidConfig.color_between;
-                    break;
-                case Type.Moon:
-                    color_between = Objects.MoonConfig.color_between;
-                    break;
-                case Type.Satellite:
-                    color_between = Objects.SatelliteConfig.color_between;
-                    break;
-                case Type.SpaceStation:
-                    color_between = Objects.SpaceStationConfig.color_between;
-                    break;
-                default:
-                    color_between = new Vector2(1.0f, 1.0f);
-                    break;
-            }
+            Vector2 color_between = type.defs().color_between;
             return Rand.Range(color_between.x, color_between.y);
         }
 
         public static float rotation_angle(this Type type) {
-            bool random_rotation;
-            switch (type) {
-                case Type.Asteroid:
-                    random_rotation = Objects.AsteroidConfig.random_rotation;
-                    break;
-                case Type.AsteroidOre:
-                    random_rotation = Objects.AsteroidConfig.random_rotation;
-                    break;
-                case Type.AsteroidCrashing:
-                    random_rotation = Objects.AsteroidConfig.random_rotation;
-                    break;
-                case Type.Moon:
-                    random_rotation = Objects.MoonConfig.random_rotation;
-                    break;
-                case Type.Satellite:
-                    random_rotation = Objects.SatelliteConfig.random_rotation;
-                    break;
-                case Type.SpaceStation:
-                    random_rotation = Objects.SpaceStationConfig.random_rotation;
-                    break;
-                default:
-                    random_rotation = false;
-                    break;
-            }
-            return random_rotation ? UnityEngine.Random.value * 360.0f : 270.0f;
+            return type.defs().random_rotation ? UnityEngine.Random.value * 360.0f : 270.0f;
         }
 
         public static int orbit_direction(this Type type) {
-            bool random_direction;
-            switch (type) {
-                case Type.Asteroid:
-                    random_direction = Objects.AsteroidConfig.random_direction;
-                    break;
-                case Type.AsteroidOre:
-                    random_direction = Objects.AsteroidConfig.random_direction;
-                    break;
-                case Type.AsteroidCrashing:
-                    random_direction = Objects.AsteroidConfig.random_direction;
-                    break;
-                case Type.Moon:
-                    random_direction = Objects.MoonConfig.random_direction;
-                    break;
-                case Type.Satellite:
-                    random_direction = Objects.SatelliteConfig.random_direction;
-                    break;
-                case Type.SpaceStation:
-                    random_direction = Objects.SpaceStationConfig.random_direction;
-                    break;
-                default:
-                    random_direction = false;
-                    break;
-            }
-            return random_direction && Rand.Bool ? 1 : -1;
+            return type.defs().random_direction && Rand.Bool ? 1 : -1;
         }
     }
 }
