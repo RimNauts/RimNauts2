8cd7d44d4d0fa47eac49d018f1995a189cc34987
Sindre Eiklid <sindre0830@gmail.com>
2023-02-21 13:39:53 +0100

    Fix hide icons patch

--- Source/RimNauts2/RimNauts2/Things/HideIcons.cs
+++ Source/RimNauts2/RimNauts2/Things/HideIcons.cs
@@ -4,66 +4,20 @@ using Verse;
 using UnityEngine;
 
 namespace RimNauts2 {
-    [HarmonyPatch(typeof(RimWorld.Planet.WorldRendererUtility), nameof(RimWorld.Planet.WorldRendererUtility.HiddenBehindTerrainNow))]
-    internal static class HideIcons {
-        internal static void Postfix(Vector3 pos, ref bool __result) {
-            Vector3 center = Find.WorldCameraDriver.CurrentlyLookingAtPointOnSphere;
-            // ignore icons on surface (settlements)
-            if (Vector3.Distance(pos, center) < 110) return;
-            float alt_percent = Find.WorldCameraDriver.AltitudePercent;
-            // ignore satellites when zoomed in
-            if (alt_percent < 0.22) {
-                __result = true;
-                return;
-            }
-            // reduce degree of visability dependent on zoom
-            float degree = 165 + ((15 * alt_percent) * 1.5f);
-            if (alt_percent > 0.30 && alt_percent < 0.35) degree = 173;
-            if (alt_percent > 0.35 && alt_percent < 0.37) degree = 174;
-            if (alt_percent > 0.37 && alt_percent < 0.46) degree = 175;
-            if (alt_percent > 0.46 && alt_percent < 0.49) degree = 176;
-            if (alt_percent > 0.49 && alt_percent < 0.53) degree = 177;
-            if (alt_percent > 0.53 && alt_percent < 0.63) degree = 178;
-            if (alt_percent > 0.63 && alt_percent < 0.87) degree = 179;
-            if (alt_percent > 0.87 && alt_percent < 0.93) degree = 179.5f;
-            if (degree < 160 || alt_percent < 0.24) degree = 160;
-            if (degree > 180) degree = 180;
+    [HarmonyPatch(typeof(RimWorld.Planet.WorldObjectSelectionUtility), nameof(RimWorld.Planet.WorldObjectSelectionUtility.HiddenBehindTerrainNow))]
+    internal static class WorldObjectSelectionUtility_HiddenBehindTerrainNow {
+        internal static bool Prefix(RimWorld.Planet.WorldObject o, ref bool __result) {
+            if (!(o is World.ObjectHolder)) return true;
+            Vector3 pos = o.DrawPos;
+            Vector3 normalized = pos.normalized;
+            float mag = pos.magnitude;
             float min_alt = 125;
             float max_alt = 1100;
-            float alt = alt_percent * (max_alt - min_alt) + max_alt;
-            __result = Vector3.Angle(pos.normalized, center) > (Math.Acos(115 / alt) + Math.Acos(115 / pos.magnitude)) * (degree / 3.14);
-            if (pos.magnitude < 115) __result = Vector3.Angle(pos.normalized, center) > 73f;
-        }
-    }
-
-    /*[HarmonyPatch(typeof(RimWorld.Planet.WorldObjectSelectionUtility), nameof(RimWorld.Planet.WorldObjectSelectionUtility.HiddenBehindTerrainNow))]
-    internal static class HideIcons {
-        internal static bool Prefix(RimWorld.Planet.WorldObject o, ref bool __result) {
-            if (o.Tile == 1) Log.Message("HEYO");
-            if (!(o is Manager)) {
-                return true;
-            }
-            __result = false;
+            float alt = Find.WorldCameraDriver.AltitudePercent * (max_alt - min_alt) + max_alt;
+            Vector3 center = Find.WorldCameraDriver.CurrentlyLookingAtPointOnSphere;
+            __result = Vector3.Angle(normalized, center) > (Math.Acos(115 / alt) + Math.Acos(115 / mag)) * (180 / 3.14d);
+            if (mag < 115) __result = Vector3.Angle(normalized, center) > 73.0f;
             return false;
         }
-    }*/
-
-    /*[HarmonyPatch(typeof(RimWorld.Planet.WorldGrid), nameof(RimWorld.Planet.WorldGrid.GetTileCenter))]
-    public class RimNauts2_WorldGrid_GetTileCenter {
-        public static bool Prefix(int tileID, ref Vector3 __result) {
-            if (RimNauts_GameComponent.satellites.TryGetValue(tileID, out Satellite satellite)) {
-                __result = satellite.DrawPos;
-                return false;
-            }
-            return true;
-        }
     }
-
-    [HarmonyPatch(typeof(RimWorld.Planet.PlanetShapeGenerator), "DoGenerate")]
-    public class RimNauts2_PlanetShapeGenerator_DoGenerate {
-        public static bool Prefix() {
-            SatelliteContainer.reset();
-            return true;
-        }
-    }*/
 }
