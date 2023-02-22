d09dcafbf5fe65c0e6937c65bb92a432fbc69833
Sindre Eiklid <sindre0830@gmail.com>
2023-02-22 17:35:26 +0100

    Simplify Thing patches

---
+++ Source/RimNauts2/RimNauts2/Things/Patch/BridgeProps.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace RimNauts2.Things.Patch {
+    [HarmonyPatch(typeof(RimWorld.SectionLayer_BridgeProps), "ShouldDrawPropsBelow")]
+    class SectionLayer_BridgeProps_ShouldDrawPropsBelow {
+        public static bool Prefix(IntVec3 c, TerrainGrid terrGrid, ref bool __result) {
+            if (terrGrid.TerrainAt(c).defName == "RimNauts2_Vacuum_Platform" || terrGrid.TerrainAt(c).defName == "RimNauts2_SteelBridge") {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Source/RimNauts2/RimNauts2/Things/Patch/PodLauncher.cs
+++ Source/RimNauts2/RimNauts2/Things/Patch/PodLauncher.cs
@@ -2,9 +2,9 @@
 using UnityEngine;
 using Verse;
 
-namespace RimNauts2 {
+namespace RimNauts2.Things.Patch {
     [HarmonyLib.HarmonyPatch(typeof(RimWorld.FuelingPortUtility), "GetFuelingPortCell", new Type[] { typeof(Building) })]
-    public static class FuelingPortUtility_GetFuelingPortCell {
+    class FuelingPortUtility_GetFuelingPortCell {
         public static bool Prefix(Building podLauncher, ref IntVec3 __result) {
             if (podLauncher.def.defName != "RimNauts2_PodLauncher") return true;
             __result = podLauncher.Position;
@@ -13,14 +13,13 @@ namespace RimNauts2 {
     }
 
     [HarmonyLib.HarmonyPatch(typeof(RimWorld.PlaceWorker_FuelingPort), "DrawGhost")]
-    public static class PlaceWorker_FuelingPort_DrawGhost {
+    class PlaceWorker_FuelingPort_DrawGhost {
         public static bool Prefix(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null) {
             if (def.defName != "RimNauts2_PodLauncher") return true;
-            Map currentMap = Find.CurrentMap;
-            rot = Rot4.South;
+            if (def.building == null || !def.building.hasFuelingPort) return false;
             center.x++;
-            if (def.building == null || !def.building.hasFuelingPort || !RimWorld.FuelingPortUtility.GetFuelingPortCell(center, rot).Standable(currentMap)) return false;
-            RimWorld.PlaceWorker_FuelingPort.DrawFuelingPortCell(center, rot);
+            if (!RimWorld.FuelingPortUtility.GetFuelingPortCell(center, Rot4.South).Standable(Find.CurrentMap)) return false;
+            RimWorld.PlaceWorker_FuelingPort.DrawFuelingPortCell(center, Rot4.South);
             return false;
         }
     }
