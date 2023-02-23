9387e83c273faf41fc26bac0ddeef22d92ae95cb
Sindre Eiklid <sindre0830@gmail.com>
2023-02-23 12:04:40 +0100

    Add texture loader in Assets

--- Source/RimNauts2/RimNauts2/Assets.cs
+++ Source/RimNauts2/RimNauts2/Assets.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace RimNauts2 {
@@ -6,10 +7,18 @@ namespace RimNauts2 {
     public static class Assets {
         private static AssetBundle assets;
         public static Shader neo_shader;
+        public static Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
 
         public static void init() {
             get_assets();
             neo_shader = get_asset("neo.shader", ShaderDatabase.WorldOverlayCutout);
+            foreach (var (type, object_holders) in Defs.Loader.object_holders) {
+                foreach (var object_holder in object_holders) {
+                    if (object_holder.texture_overlay != null && !textures.ContainsKey(object_holder.texture_overlay)) {
+                        textures.Add(object_holder.texture_overlay, get_content<Texture2D>(object_holder.texture_overlay));
+                    }
+                }
+            }
         }
 
         private static void get_assets() {
@@ -53,9 +62,19 @@ namespace RimNauts2 {
             );
         }
 
-        public static T get_asset<T>(string name, T fallback) where T : Object {
+        public static T get_asset<T>(string name, T fallback = null) where T : Object {
             if (assets == null) return fallback;
             return assets.LoadAsset<T>(name);
+        }
+
+        public static T get_content<T>(string path, T fallback = null) where T : Object {
+            if (assets == null) return fallback;
+            return ContentFinder<T>.Get(path);
+        }
+
+        public static Texture2D get_texture(string path) {
+            if (path == null || !textures.ContainsKey(path)) return null;
+            return textures[path];
         }
     }
 }
