4b7fc095a32ee7328c2f0d72eed7b927dd44ff32
Sindre Eiklid <sindre0830@gmail.com>
2023-02-27 14:24:38 +0100

    Bump version

--- Source/RimNauts2/RimNauts2/Constants.cs
+++ Source/RimNauts2/RimNauts2/Constants.cs
@@ -1,7 +1,7 @@
 ﻿namespace RimNauts2 {
     public static class Info {
         public static readonly string name = "RimNauts 2";
-        public static readonly string version = "4.1.0";
+        public static readonly string version = "4.2.0";
     }
 
     public static class Style {
