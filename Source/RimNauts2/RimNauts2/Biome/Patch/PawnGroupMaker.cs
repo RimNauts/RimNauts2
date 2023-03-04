f96edd0559a6bf9c7faaea97390346e9b451e96f
Sindre Eiklid <sindre0830@gmail.com>
2023-03-04 11:33:02 +0100

    Update PawnGroupMaker patch

--- Source/RimNauts2/RimNauts2/Biome/Patch/PawnGroupMaker.cs
+++ Source/RimNauts2/RimNauts2/Biome/Patch/PawnGroupMaker.cs
@@ -6,16 +6,23 @@ namespace RimNauts2.Biome.Patch {
     [HarmonyLib.HarmonyPatch(typeof(RimWorld.PawnGroupMakerUtility), "GeneratePawns")]
     class PawnGroupMakerUtility_GeneratePawns {
         public static IEnumerable<Pawn> Postfix(IEnumerable<Pawn> __result, RimWorld.PawnGroupMakerParms parms, bool warnOnZeroResults) {
-            if (__result == null || !__result.Any()) yield break;
-            bool no_oxygen = Universum.Utilities.Cache.allowed_utility(Find.WorldGrid[parms.tile].biome, "universum.vacuum_suffocation");
-            bool decompression = Universum.Utilities.Cache.allowed_utility(Find.WorldGrid[parms.tile].biome, "universum.vacuum_decompression");
+            RimWorld.BiomeDef biome = Find.WorldGrid[parms.tile].biome;
+            bool no_oxygen = Universum.Utilities.Cache.allowed_utility(biome, "universum.vacuum_suffocation");
+            bool decompression = Universum.Utilities.Cache.allowed_utility(biome, "universum.vacuum_decompression");
             bool requires_spacesuit = no_oxygen || decompression;
-            foreach (Pawn pawn in __result) {
-                if (requires_spacesuit && pawn != null) {
-                    RimWorld.Apparel space_helmet = (RimWorld.Apparel) ThingMaker.MakeThing(ThingDef.Named("RimNauts2_Apparel_SpaceSuit_Head"));
-                    RimWorld.Apparel space_suit = (RimWorld.Apparel) ThingMaker.MakeThing(ThingDef.Named("RimNauts2_Apparel_SpaceSuit_Body"));
-                    pawn.apparel.Wear(space_helmet, false);
-                    pawn.apparel.Wear(space_suit, false);
+            if (requires_spacesuit) {
+                return apply_spacesuits(__result);
+            } else return __result;
+        }
+
+        public static IEnumerable<Pawn> apply_spacesuits(IEnumerable<Pawn> pawns) {
+            if (pawns == null || !pawns.Any()) yield break;
+            ThingDef spacesuit_helmet = ThingDef.Named("RimNauts2_Apparel_SpaceSuit_Head");
+            ThingDef spacesuit_armor = ThingDef.Named("RimNauts2_Apparel_SpaceSuit_Body");
+            foreach (Pawn pawn in pawns) {
+                if (pawn != null) {
+                    pawn.apparel.Wear((RimWorld.Apparel) ThingMaker.MakeThing(spacesuit_helmet), false);
+                    pawn.apparel.Wear((RimWorld.Apparel) ThingMaker.MakeThing(spacesuit_armor), false);
                 }
                 yield return pawn;
             }
