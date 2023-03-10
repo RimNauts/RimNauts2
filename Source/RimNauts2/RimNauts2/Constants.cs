8971a5758694e38af5f98ccb4d4016b3502d85f2
Sindre Eiklid <sindre0830@gmail.com>
2023-03-10 23:22:48 +0100

    Move space station wall and floor thingdefs to the general def of

--- Source/RimNauts2/RimNauts2/Constants.cs
+++ Source/RimNauts2/RimNauts2/Constants.cs
@@ -1,7 +1,7 @@
 ﻿namespace RimNauts2 {
     public static class Info {
         public static readonly string name = "RimNauts 2";
-        public static readonly string version = "4.5.2";
+        public static readonly string version = "4.6.0";
     }
 
     public static class Style {
