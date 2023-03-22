d044278c4595fe27656baf92ea9461ddc598e63f
Sindre Eiklid <sindre0830@gmail.com>
2023-03-22 19:19:44 +0100

    Fix storage system for Delivery Cannon

--- Source/RimNauts2/RimNauts2/Things/Building/DeliveryCannon.cs
+++ Source/RimNauts2/RimNauts2/Things/Building/DeliveryCannon.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Verse;
 
 namespace RimNauts2.Things.Building {
     [StaticConstructorOnStartup]
     public class DeliveryCannon : RimWorld.Building_Storage {
+        int launch_interval = 400;
+        float max_capacity = 1000;
 
+        public override void Tick() {
+            base.Tick();
+            if (World.RenderingManager.tick % launch_interval != 0) return;
+            launch();
+        }
+
+        private void launch() {
+            List<Thing> things = new List<Thing>();
+            float capacity = 0;
+            
+            foreach (Thing thing in slotGroup.HeldThings) {
+                if (capacity >= max_capacity) return;
+                Log.Message("BAM: " + thing.def.defName);
+                capacity += thing.def.BaseMass;
+                things.Add(thing);
+            }
+        }
     }
 }
