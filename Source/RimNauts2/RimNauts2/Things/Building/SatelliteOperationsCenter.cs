6bde206fa0630ea7b02c9dad85109dd7dbafa035
Sindre Eiklid <sindre0830@gmail.com>
2023-11-07 21:00:21 +0100

    Add energy relay satellite to operations center

--- Source/RimNauts2/RimNauts2/Things/Building/SatelliteOperationsCenter.cs
+++ Source/RimNauts2/RimNauts2/Things/Building/SatelliteOperationsCenter.cs
@@ -5,18 +5,31 @@ using Verse;
 
 namespace RimNauts2.Things.Building {
     public class SatelliteOperationsCenter : Verse.Building {
+        private string descConnection = "Connecting...";
+
         private Universum.Defs.CelestialObject _relaySatelliteDef;
-        private Command _relaySatelliteCmd;
         private Texture2D _relaySatelliteIcon;
+        private Command _relaySatelliteCmd;
         private int _relayTotal = 0;
         private int _relayTotalNeeded = 0;
-        private string descConnection = "Connecting...";
+
+        private Universum.Defs.CelestialObject _energyRelaySatelliteDef;
+        private Texture2D _energyRelaySatelliteIcon;
+        private Command _energyRelaySatelliteCmd;
+        private int _energyRelayTotal = 0;
+        private int _energyRelayCapacity = 0;
+        private int _powerGenerated = 0;
+        private int _powerNeeded = 0;
 
         public SatelliteOperationsCenter() {
             _relaySatelliteDef = Universum.Defs.Loader.celestialObjects["RimNauts2_CelestialObject_Satellite_Relay"];
             _relaySatelliteIcon = Universum.Assets.GetTexture(_relaySatelliteDef.icon.texturePath);
 
+            _energyRelaySatelliteDef = Universum.Defs.Loader.celestialObjects["RimNauts2_CelestialObject_Satellite_Energy"];
+            _energyRelaySatelliteIcon = Universum.Assets.GetTexture(_energyRelaySatelliteDef.icon.texturePath);
+
             _UpdateRelaySatelliteCmd();
+            _UpdateEnergyRelaySatelliteCmd();
         }
 
         public override string GetInspectString() {
@@ -31,8 +44,14 @@ namespace RimNauts2.Things.Building {
             // update properties
             _relayTotal = Universum.Game.MainLoop.instance.GetTotal(_relaySatelliteDef);
             _relayTotalNeeded = 0;
+
+            _energyRelayTotal = Universum.Game.MainLoop.instance.GetTotal(_energyRelaySatelliteDef);
+            _energyRelayCapacity = _energyRelayTotal * 50;
+            _powerGenerated = _energyRelayTotal * 20;
+            _powerNeeded = 0;
             // update commands
             _UpdateRelaySatelliteCmd();
+            _UpdateEnergyRelaySatelliteCmd();
             // update description
             descConnection = "Connected";
         }
@@ -43,15 +62,34 @@ namespace RimNauts2.Things.Building {
             }
 
             yield return _relaySatelliteCmd;
+            yield return _energyRelaySatelliteCmd;
         }
 
         private void _void() { }
 
         private void _UpdateRelaySatelliteCmd() {
+            string desc = "";
+            desc += $"Total: {_relayTotal}\n";
+            desc += $"Needed: {_relayTotalNeeded}";
+
             _relaySatelliteCmd = new Command_Action {
                 defaultLabel = "Relay Satellite",
-                defaultDesc = $"Total: \t{_relayTotal}\nNeeded: \t{_relayTotalNeeded}",
+                defaultDesc = desc,
                 icon = _relaySatelliteIcon,
+                action = new Action(_void)
+            };
+        }
+
+        private void _UpdateEnergyRelaySatelliteCmd() {
+            string desc = "";
+            desc += $"Total: {_energyRelayTotal}\n";
+            desc += $"Relay Capacity: {_energyRelayCapacity}\n";
+            desc += $"Power Needed/Generated: {_powerNeeded}/{_powerGenerated}";
+
+            _energyRelaySatelliteCmd = new Command_Action {
+                defaultLabel = "Energy Relay Satellite",
+                defaultDesc = desc,
+                icon = _energyRelaySatelliteIcon,
                 action = new Action(_void)
             };
         }
