e227b5b286a0ed8922628b58725fe5a85e25813f
Sindre Eiklid <sindre0830@gmail.com>
2023-11-10 19:07:32 +0100

    Add Cosmic Surveillance Satellite

--- Source/RimNauts2/RimNauts2/Things/Building/SatelliteOperationsCenter.cs
+++ Source/RimNauts2/RimNauts2/Things/Building/SatelliteOperationsCenter.cs
@@ -21,6 +21,12 @@ namespace RimNauts2.Things.Building {
         private int _powerGenerated = 0;
         private int _powerNeeded = 0;
 
+        private Universum.Defs.CelestialObject _cosmicSurveillanceSatelliteDef;
+        private Texture2D _cosmicSurveillanceSatelliteIcon;
+        private Command _cosmicSurveillanceSatelliteCmd;
+        private int _cosmicSurveillanceTotal = 0;
+        private int _cosmicSurveillancePowerConsumption = 0;
+
         public SatelliteOperationsCenter() {
             _relaySatelliteDef = Universum.Defs.Loader.celestialObjects["RimNauts2_CelestialObject_Satellite_Relay"];
             _relaySatelliteIcon = Universum.Assets.GetTexture(_relaySatelliteDef.icon.texturePath);
@@ -28,8 +34,12 @@ namespace RimNauts2.Things.Building {
             _energyRelaySatelliteDef = Universum.Defs.Loader.celestialObjects["RimNauts2_CelestialObject_Satellite_Energy"];
             _energyRelaySatelliteIcon = Universum.Assets.GetTexture(_energyRelaySatelliteDef.icon.texturePath);
 
+            _cosmicSurveillanceSatelliteDef = Universum.Defs.Loader.celestialObjects["RimNauts2_CelestialObject_Satellite_CosmicSurveillance"];
+            _cosmicSurveillanceSatelliteIcon = Universum.Assets.GetTexture(_cosmicSurveillanceSatelliteDef.icon.texturePath);
+
             _UpdateRelaySatelliteCmd();
             _UpdateEnergyRelaySatelliteCmd();
+            _UpdateCosmicSurveillanceSatelliteCmd();
         }
 
         public override string GetInspectString() {
@@ -42,16 +52,23 @@ namespace RimNauts2.Things.Building {
         public override void TickRare() {
             base.TickRare();
             // update properties
-            _relayTotal = Universum.Game.MainLoop.instance.GetTotal(_relaySatelliteDef);
             _relayTotalNeeded = 0;
+            _powerNeeded = 0;
+
+            _relayTotal = Universum.Game.MainLoop.instance.GetTotal(_relaySatelliteDef);
 
             _energyRelayTotal = Universum.Game.MainLoop.instance.GetTotal(_energyRelaySatelliteDef);
             _energyRelayCapacity = _energyRelayTotal * 50;
             _powerGenerated = _energyRelayTotal * 20;
-            _powerNeeded = 0;
+
+            _cosmicSurveillanceTotal = Universum.Game.MainLoop.instance.GetTotal(_cosmicSurveillanceSatelliteDef);
+            _cosmicSurveillancePowerConsumption = _cosmicSurveillanceTotal * 50;
+            _relayTotalNeeded += _cosmicSurveillanceTotal;
+            _powerNeeded += _cosmicSurveillancePowerConsumption;
             // update commands
             _UpdateRelaySatelliteCmd();
             _UpdateEnergyRelaySatelliteCmd();
+            _UpdateCosmicSurveillanceSatelliteCmd();
             // update description
             descConnection = "Connected";
         }
@@ -63,6 +80,7 @@ namespace RimNauts2.Things.Building {
 
             yield return _relaySatelliteCmd;
             yield return _energyRelaySatelliteCmd;
+            yield return _cosmicSurveillanceSatelliteCmd;
         }
 
         private void _void() { }
@@ -78,18 +96,36 @@ namespace RimNauts2.Things.Building {
                 icon = _relaySatelliteIcon,
                 action = new Action(_void)
             };
+
+            if (_relayTotalNeeded > _relayTotal) _relaySatelliteCmd.defaultIconColor = Color.red;
         }
 
         private void _UpdateEnergyRelaySatelliteCmd() {
             string desc = "";
             desc += $"Total: {_energyRelayTotal}\n";
-            desc += $"Relay Capacity: {_energyRelayCapacity}\n";
-            desc += $"Power Needed/Generated: {_powerNeeded}/{_powerGenerated}";
+            desc += $"Energy Relay Capacity: {_energyRelayCapacity}\n";
+            desc += $"Power Needed/Generated: {_powerNeeded}/{_powerGenerated}\n";
+            desc += $"Excess Power: {_powerGenerated - _powerNeeded}";
 
             _energyRelaySatelliteCmd = new Command_Action {
                 defaultLabel = "Energy Relay Satellite",
                 defaultDesc = desc,
                 icon = _energyRelaySatelliteIcon,
+                action = new Action(_void)
+            };
+
+            if (_powerNeeded > _powerGenerated || _powerNeeded > _energyRelayCapacity) _energyRelaySatelliteCmd.defaultIconColor = Color.red;
+        }
+
+        private void _UpdateCosmicSurveillanceSatelliteCmd() {
+            string desc = "";
+            desc += $"Total: {_cosmicSurveillanceTotal}\n";
+            desc += $"Total Power Consumption: {_cosmicSurveillancePowerConsumption}";
+
+            _cosmicSurveillanceSatelliteCmd = new Command_Action {
+                defaultLabel = "Cosmic Surveillanc Satellite",
+                defaultDesc = desc,
+                icon = _cosmicSurveillanceSatelliteIcon,
                 action = new Action(_void)
             };
         }
