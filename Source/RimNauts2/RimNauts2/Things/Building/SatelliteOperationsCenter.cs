bfad6394506159db0381dc99febe4a7a2841cb92
Sindre Eiklid <sindre0830@gmail.com>
2023-11-12 13:35:50 +0100

    Update Satellite Operation Center UI

--- Source/RimNauts2/RimNauts2/Things/Building/SatelliteOperationsCenter.cs
+++ Source/RimNauts2/RimNauts2/Things/Building/SatelliteOperationsCenter.cs
@@ -11,6 +11,7 @@ namespace RimNauts2.Things.Building {
         private Texture2D _relaySatelliteIcon;
         private Command _relaySatelliteCmd;
         private int _relayTotal = 0;
+        private int _relayCapacity = 0;
         private int _relayTotalNeeded = 0;
 
         private Universum.Defs.CelestialObject _energyRelaySatelliteDef;
@@ -56,6 +57,7 @@ namespace RimNauts2.Things.Building {
             _powerNeeded = 0;
 
             _relayTotal = Universum.Game.MainLoop.instance.GetTotal(_relaySatelliteDef);
+            _relayCapacity = _relayTotal * 4;
 
             _energyRelayTotal = Universum.Game.MainLoop.instance.GetTotal(_energyRelaySatelliteDef);
             _energyRelayCapacity = _energyRelayTotal * 50;
@@ -83,51 +85,83 @@ namespace RimNauts2.Things.Building {
             yield return _cosmicSurveillanceSatelliteCmd;
         }
 
-        private void _void() { }
+        private void _Void() { }
 
         private void _UpdateRelaySatelliteCmd() {
-            string desc = "";
-            desc += $"Total: {_relayTotal}\n";
-            desc += $"Needed: {_relayTotalNeeded}";
+            bool needCapacity = _relayTotalNeeded > _relayCapacity;
+
+            bool problemDetected = needCapacity;
+
+            string desc = _BuildHeader("Relay Satellite");
+            desc += _BuildOverview("The Relay Satellite is vital for satellite signal transmission, requiring enough units for communication needs.");
+            desc += _AddDetail($"Total Satellites: {_relayTotal}");
+            desc += _AddDetail($"Relay Demand/Capacity: {_relayTotalNeeded}/{_relayCapacity} ({_GetSurplusString(surplus: _relayCapacity - _relayTotalNeeded)})");
+            if (problemDetected) desc += _AddAlertHeader("ALERT");
+            if (needCapacity) desc += _AddAlertDetail(reason: "Limited relay capacity.", solution: "Deploy additional Relay Satellites to increase capacity.");
 
             _relaySatelliteCmd = new Command_Action {
                 defaultLabel = "Relay Satellite",
                 defaultDesc = desc,
                 icon = _relaySatelliteIcon,
-                action = new Action(_void)
+                action = new Action(_Void)
             };
 
-            if (_relayTotalNeeded > _relayTotal) _relaySatelliteCmd.defaultIconColor = Color.red;
+            if (problemDetected) _relaySatelliteCmd.defaultIconColor = Color.red;
         }
 
         private void _UpdateEnergyRelaySatelliteCmd() {
-            string desc = "";
-            desc += $"Total: {_energyRelayTotal}\n";
-            desc += $"Energy Relay Capacity: {_energyRelayCapacity}\n";
-            desc += $"Power Needed/Generated: {_powerNeeded}/{_powerGenerated}\n";
-            desc += $"Excess Power: {_powerGenerated - _powerNeeded}";
+            bool needPower = _powerNeeded > _powerGenerated;
+            bool needCapacity = _powerNeeded > _energyRelayCapacity;
+
+            bool problemDetected = needPower || needCapacity;
+
+            string desc = _BuildHeader("Energy Relay Satellite");
+            desc += _BuildOverview("Key for power transmission, this satellite is essential for managing your network's power distribution.");
+            desc += _AddDetail($"Total Satellites: {_energyRelayTotal}");
+            desc += _AddDetail($"Relay Demand/Capacity: {_powerNeeded}/{_energyRelayCapacity} ({_GetSurplusString(surplus: _energyRelayCapacity - _powerNeeded)})");
+            desc += _AddDetail($"Power Demand/Supply: {_powerNeeded}/{_powerGenerated} ({_GetSurplusString(surplus: _powerGenerated - _powerNeeded)})");
+            if (problemDetected) desc += _AddAlertHeader("ALERT");
+            if (needPower) desc += _AddAlertDetail(reason: "Power deficit detected.", solution: "Increase power generation by deploying Solar Grid Satellites.");
+            if (needCapacity) desc += _AddAlertDetail(reason: "Limited relay capacity.", solution: "Deploy additional Energy Relay Satellites to increase capacity.");
 
             _energyRelaySatelliteCmd = new Command_Action {
                 defaultLabel = "Energy Relay Satellite",
                 defaultDesc = desc,
                 icon = _energyRelaySatelliteIcon,
-                action = new Action(_void)
+                action = new Action(_Void)
             };
 
-            if (_powerNeeded > _powerGenerated || _powerNeeded > _energyRelayCapacity) _energyRelaySatelliteCmd.defaultIconColor = Color.red;
+            if (problemDetected) _energyRelaySatelliteCmd.defaultIconColor = Color.red;
         }
 
         private void _UpdateCosmicSurveillanceSatelliteCmd() {
-            string desc = "";
-            desc += $"Total: {_cosmicSurveillanceTotal}\n";
-            desc += $"Total Power Consumption: {_cosmicSurveillancePowerConsumption}";
+            string desc = _BuildHeader("Cosmic Surveillanc Satellite");
+            desc += _BuildOverview("The Cosmic Surveillance Satellite is used for expansive space surveillance. It enhances vision in space and paves the way for discovering celestial objects.");
+            desc += _AddDetail($"Total Satellites: {_cosmicSurveillanceTotal}");
+            desc += _AddDetail($"Total Power Consumption: {_cosmicSurveillancePowerConsumption}");
 
             _cosmicSurveillanceSatelliteCmd = new Command_Action {
                 defaultLabel = "Cosmic Surveillanc Satellite",
                 defaultDesc = desc,
                 icon = _cosmicSurveillanceSatelliteIcon,
-                action = new Action(_void)
+                action = new Action(_Void)
             };
+        }
+
+        private string _BuildHeader(string body) => "<size=15><b>" + body + "</b></size>\n";
+
+        private string _BuildOverview(string body) => "\n" + body + "\n";
+
+        private string _AddDetail(string body) => "\n\n" + body;
+
+        private string _AddAlertHeader(string body) => "\n\n\n" + body.Colorize(ColorLibrary.RedReadable);
+
+        private string _AddAlertDetail(string reason, string solution) => "\n\n" + "<b>" + reason + "</b> <i>" + solution + "</i>";
+
+        private string _GetSurplusString(int surplus) {
+            if (surplus < 0) {
+                return surplus.ToString().Colorize(ColorLibrary.RedReadable);
+            } else return surplus.ToString().Colorize(ColorLibrary.Green);
         }
     }
 }
